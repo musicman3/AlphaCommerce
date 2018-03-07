c1640818f6681409c96296d36d9437a11c43875f
musicman3 <micromail@mail.ru>
2018-03-08 01:56:30 +0200

    Фикс корзины

--- includes/content/products/products.php
+++ includes/content/products/products.php
@@ -12,62 +12,63 @@
   as published by the Free Software Foundation.
 */
 
-  class osC_Products_Products extends osC_Template {
+    class osC_Products_Products extends osC_Template {
 
-/* Private variables */
+        /* Private variables */
 
-    var $_module = 'products',
+        var $_module = 'products',
         $_group = 'products',
         $_page_title,
         $_page_contents = 'info.php',
         $_page_image = 'table_background_list.gif';
 
-/* Class constructor */
+        /* Class constructor */
 
-    function osC_Products_Products() {
-      global $osC_Database, $osC_Services, $osC_Session, $osC_Language, $osC_Breadcrumb, $osC_Product;
+        function osC_Products_Products() {
+            global $osC_Database, $osC_Services, $osC_Session, $osC_Language, $osC_Breadcrumb, $osC_Product;
 
-      if (empty($_GET) === false) {
-        $id = false;
+            if (empty($_GET) === false) {
+                $id = false;
 
-// PHP < 5.0.2; array_slice() does not preserve keys and will not work with numerical key values, so foreach() is used
-        foreach ($_GET as $key => $value) {
-          if ( (preg_match('/^[0-9]+(#?([0-9]+:?[0-9]+)+(;?([0-9]+:?[0-9]+)+)*)*$/', $key) || preg_match('/^[a-zA-Z0-9 -_]*$/', $key)) && ($key != $osC_Session->getName()) ) {//if ( (ereg('^[0-9]+(#?([0-9]+:?[0-9]+)+(;?([0-9]+:?[0-9]+)+)*)*$', $key) || ereg('^[a-zA-Z0-9 -_]*$', $key)) && ($key != $osC_Session->getName()) ) {
-            $id = $key;
-          }
+                foreach ($_GET as $key => $value) {
+                    if ( (preg_match('/^[0-9]+(#?([0-9]+:?[0-9]+)+(;?([0-9]+:?[0-9]+)+)*)*$/', $key) || preg_match('/^[a-zA-Z0-9 -_]*$/', $key)) && ($key != $osC_Session->getName()) ) {
+                        $id = $key;
+                    }
 
-          break;
+                    break;
+                }
+
+                $osC_Product_Temp = new osC_Product($id);
+
+                if (($id !== false) && $osC_Product_Temp->checkEntry($id)) {
+                    $osC_Product = new osC_Product($id);
+                    $osC_Product->incrementCounter();
+
+                    $this->addPageTags('keywords', $osC_Product->getTitle());
+                    $this->addPageTags('keywords', $osC_Product->getModel());
+
+                    if ($osC_Product->hasTags()) {
+                        $this->addPageTags('keywords', $osC_Product->getTags());
+                    }
+
+                    $this->addJavascriptFilename('templates/' . $this->getCode() . '/javascript/' . $this->_group . '/info.js');
+
+                    $osC_Services_category_path = new osC_Services_category_path();
+                    $osC_Services_category_path->process($osC_Product->getCategoryID());
+
+                    if ($osC_Services->isStarted('breadcrumb')) {
+                        $osC_Breadcrumb->add($osC_Product->getTitle(), osc_href_link(FILENAME_PRODUCTS, $osC_Product->getKeyword()));
+                    }
+
+                    $this->_page_title = $osC_Product->getTitle();
+                } else {
+                    $this->_page_title = $osC_Language->get('product_not_found_heading');
+                    $this->_page_contents = 'info_not_found.php';
+                }
+            } else {
+                $this->_page_title = $osC_Language->get('product_not_found_heading');
+                $this->_page_contents = 'info_not_found.php';
+            }
         }
-
-          $osC_Product = new osC_Product($id);
-        if (($id !== false) && $osC_Product->checkEntry($id)) {
-          $osC_Product->incrementCounter();
-
-          $this->addPageTags('keywords', $osC_Product->getTitle());
-          $this->addPageTags('keywords', $osC_Product->getModel());
-
-          if ($osC_Product->hasTags()) {
-            $this->addPageTags('keywords', $osC_Product->getTags());
-          }
-
-          $this->addJavascriptFilename('templates/' . $this->getCode() . '/javascript/' . $this->_group . '/info.js');
-
-          $osC_Services_category_path = new osC_Services_category_path();
-          $osC_Services_category_path->process($osC_Product->getCategoryID());
-
-          if ($osC_Services->isStarted('breadcrumb')) {
-            $osC_Breadcrumb->add($osC_Product->getTitle(), osc_href_link(FILENAME_PRODUCTS, $osC_Product->getKeyword()));
-          }
-
-          $this->_page_title = $osC_Product->getTitle();
-        } else {
-          $this->_page_title = $osC_Language->get('product_not_found_heading');
-          $this->_page_contents = 'info_not_found.php';
-        }
-      } else {
-        $this->_page_title = $osC_Language->get('product_not_found_heading');
-        $this->_page_contents = 'info_not_found.php';
-      }
     }
-  }
 ?>
